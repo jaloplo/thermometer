23fc393341ff119254aaaa638b0436d439e74eea
Jaime <jaime.lopez.lopez@live.com>
2019-08-01 18:32:59 +0200

    * Added first UI module implementation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
-var thermometerApp = new Vue({
+const SCALES_AVAILABLE = ['Celsius', 'Farenheit', 'Kelvin'];
+
+let model = {
+    scales: SCALES_AVAILABLE,
+    current: {
+        temperature: 14,
+        scale: SCALES_AVAILABLE[0]
+    }
+};
+
+let thermometerApp = new Vue({
     el: '#app',
-    data: {
-        message: 'Hello Thermometer app'
+    data: model,
+    methods: {
+        changeScaleTo: function(newScale) {
+            console.log('changing scale to ' + newScale);
+        }
     }
 });
