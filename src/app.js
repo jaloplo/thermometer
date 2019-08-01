f67df0151e81f25a7f47662e1a88fb2444624f64
Jaime <jaime.lopez.lopez@live.com>
2019-08-01 18:54:49 +0200

    + First version of the temperature converter

--- src/app.js
+++ src/app.js
@@ -1,4 +1,29 @@
-const SCALES_AVAILABLE = ['Celsius', 'Farenheit', 'Kelvin'];
+const CELSIUS_SCALE = 'Celsius';
+const FARENHEIT_SCALE = 'Farenheit';
+const KELVIN_SCALE = 'Kelvin';
+
+const SCALES_AVAILABLE = [CELSIUS_SCALE, FARENHEIT_SCALE, KELVIN_SCALE];
+
+const converters = [];
+converters.push({
+    source: CELSIUS_SCALE,
+    target: KELVIN_SCALE,
+    formula: function(degree) {
+        return degree + parseFloat(273.15);
+    }
+});
+converters.push({
+    source: KELVIN_SCALE,
+    target: CELSIUS_SCALE,
+    formula: function(degree) {
+        return degree - parseFloat(273.15);
+    }
+});
+
+let temperatureConverter = function(temperature, oldScale, newScale) {
+    return converters.filter(c => c.source === oldScale && c.target === newScale)[0].formula(temperature);
+}
+
 
 let model = {
     scales: SCALES_AVAILABLE,
@@ -13,7 +38,12 @@ let thermometerApp = new Vue({
     data: model,
     methods: {
         changeScaleTo: function(newScale) {
-            console.log('changing scale to ' + newScale);
+            console.log('changing scale from ' + thermometerApp.current.scale + ' to ' + newScale);
+            thermometerApp.current.temperature = temperatureConverter(
+                thermometerApp.current.temperature, 
+                thermometerApp.current.scale,
+                newScale);
+            thermometerApp.current.scale = newScale;
         }
     }
 });
