64fa2edc5c402d1ae942369744ec4d718dc7cd27
Jaime <jaime.lopez.lopez@live.com>
2019-08-05 01:22:35 +0200

    * Changed the temperature service as a service

--- src/app.js
+++ src/app.js
@@ -4,17 +4,24 @@ const KELVIN_SCALE = 'Kelvin';
 
 const SCALES_AVAILABLE = [CELSIUS_SCALE, FARENHEIT_SCALE, KELVIN_SCALE];
 
-const TemperatureParserHandler = function() {
+/* # TransformationService
+ * Handle transformation methods from one type to another.
+ *
+ * ## Example
+ * let ts = new TransformationService();
+ * ts.register('meter', 'centimeter', (value) => value * 100);
+ * let length = ts.get('meter', 'centimer').transform(2); // length value shoudl be 200.
+ */
+const TransformationService = function() {
     let collection = {};
 
     return {
         get: function(source, target) {
-            if(!collection[source]) {
+            if(!collection[source] || !collection[source][target]) {
                 // TODO: throw an error
                 return null;
             }
 
-            // TODO: if target doesn't exist throw an error
             return collection[source][target];
         },
 
@@ -27,24 +34,60 @@ const TemperatureParserHandler = function() {
             }
 
             collection[source][target].transform = formula;
+
+            return this;
         }
     };
 };
 
-const TemperatureParserBuilder = {
-    build: function() {
-        let parser = new TemperatureParserHandler();
-    
-        parser.register(CELSIUS_SCALE, KELVIN_SCALE, (degree) => degree + parseFloat(273.15));
-        parser.register(KELVIN_SCALE, CELSIUS_SCALE, (degree) => degree - parseFloat(273.15));
-        parser.register(CELSIUS_SCALE, FARENHEIT_SCALE, (degree) => (9*degree/5) + 32);
-        parser.register(FARENHEIT_SCALE, CELSIUS_SCALE, (degree) => (5*(degree-32)) / 9);
-        parser.register(FARENHEIT_SCALE, KELVIN_SCALE, (degree) => (5*(degree-32)) / 9 + parseFloat(273.15));
-        parser.register(KELVIN_SCALE, FARENHEIT_SCALE, (degree) => (9*(degree - 273.15)/5) + 32);
-    
-        return parser;
-    }
+/* # TemperatureTransformationService
+ * Handle temperature transformation methods from one scale to another.
+ * Current scales supported are Celsius, Fahrenheit and Kelvin.
+ *
+ * ## Example
+ * let ts = new TransformationService();
+ * let temperatureService = new TemperatureTransformationService(ts);
+ * let fahrenheit = temperatureService.get(CELSIUS_SCALE, FAHRENHEIT_SCALE, 32); // fahrenheit should be TODO:
+ */
+const TemperatureTransformationService = function(transformationService) {
+    let service = transformationService;
+
+    service.register(CELSIUS_SCALE, KELVIN_SCALE, (degree) => degree + parseFloat(273.15));
+    service.register(KELVIN_SCALE, CELSIUS_SCALE, (degree) => degree - parseFloat(273.15));
+    service.register(CELSIUS_SCALE, FARENHEIT_SCALE, (degree) => (9*degree/5) + 32);
+    service.register(FARENHEIT_SCALE, CELSIUS_SCALE, (degree) => (5*(degree-32)) / 9);
+    service.register(FARENHEIT_SCALE, KELVIN_SCALE, (degree) => (5*(degree-32)) / 9 + 273.15);
+    service.register(KELVIN_SCALE, FARENHEIT_SCALE, (degree) => (9*(degree - 273.15)/5) + 32);
+
+    return service;
 };
+
+/* # Converter
+ * Converts TransformationService methods to be easier to read.
+ * Applies currying technique in order to write code more readable to human beings.
+ * 
+ * ## Example
+ * let ts = new TransformationService();
+ * let temperatureService = new TemperatureTransformationService(ts);
+ * let convert = new Converter(temperatureService);
+ * let fahrenheit = convert(32).from(CELSIUS_SCALE).to(FAHRENHEIT_SCALE); // fahrenheit should be TODO:
+ */
+const Converter = function(service) {
+    let _service = service;
+
+    return function(value) {
+        return {
+            from: function(source) {
+                return {
+                    to: function(target) {
+                        return _service.get(source, target).transform(value);
+                    }
+                };
+            }
+        };
+    };
+};
+
 
 const PositionController = function() {
     return {
@@ -83,10 +126,29 @@ const AsyncWeatherProvider = function(weatherConnector) {
     };
 };
 
-const App = function() {
-    let temperatureParser = null;
-    let weatherConnector = null;
-    let weatherProvider = null;
+const WeatherProviderBuilder = {
+    build: function(connector, provider) {
+        return new provider(connector);
+    }
+};
+
+const AppController = function(transformer, weatherProvider) {
+    let _transformer = transformer;
+    let _weatherProvider = weatherProvider;
+
+    return {
+        changeScaleTo: function(value, source, target) {
+            return _transformer(value).from(source).to(target);
+        },
+        getCurrentTemperature: async function() {
+            let response = await _weatherProvider.get()
+            return response;
+        }
+    };
+}
+
+const App = function(controller) {
+    let _controller = controller;
     let weatherUI = null;
     
     let model = {
@@ -96,37 +158,40 @@ const App = function() {
     };
 
     return {
-        init: function() {
-            temperatureParser = TemperatureParserBuilder.build();
-            
-            weatherConnector = new FakeWeatherConnector();
-            weatherProvider = new AsyncWeatherProvider(weatherConnector);
-
+        init: function() {            
             weatherUI = new Vue({
                 el: '#app',
                 data: model,
                 methods: {
                     changeScaleTo: function(newScale) {
-                        weatherUI.current.temperature = temperatureParser
-                            .get(weatherUI.current.scale, newScale)
-                            .transform(weatherUI.current.temperature);
+                        weatherUI.current.temperature = _controller.changeScaleTo(
+                            weatherUI.current.temperature,
+                            weatherUI.current.scale,
+                            newScale);
                         weatherUI.current.scale = newScale;
                     }
                 }
             });
             return this;
         },
-        start: function() {
-            weatherProvider.get().then(res => {
-                weatherUI.current = res;
-                weatherUI.status = 1;
-            });
+        start: async function() {
+            let res = await _controller.getCurrentTemperature();
+            weatherUI.current = res;
+            weatherUI.status = 1;
             return this;
         }
     };
 };
 
-let thermometerApp = new App();
+let transformationService = new TransformationService();
+let temperatureService = new TemperatureTransformationService(transformationService);
+let convert = new Converter(temperatureService);
+ 
+let controller = new AppController(
+    convert,
+    WeatherProviderBuilder.build(new FakeWeatherConnector(), AsyncWeatherProvider));
+let thermometerApp = new App(controller);
+
 thermometerApp
     .init()
     .start();
